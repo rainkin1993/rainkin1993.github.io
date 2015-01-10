06c93c2e2d12e8d9928415fbb9670363ef632ef9
rainkin <rainkin1993@gmail.com>
2015-01-11 00:25:31 +0800

    update blog

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,20 @@
-git checkout source
-echo 1231231223
-echo 123123122
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
-    echo "push source"
+    git checkout source
+    jekyll build
+    git add -A
+    git commit -m "update source"
+
+    cp -r _site/ /tmp/
+    rm -r /tmp/_site
+    cp -r _site/ /tmp
+
+    git checkout master
+    cp -rf /tmp/_site/* ./    
+    git add -A
+    git reset HEAD .fuse_hidden*
+    git commit -m "update blog"
+    git push origin master
+
+    git checkout source
+    echo "deploy succeed"
+    git push origin source
     echo "push source"
